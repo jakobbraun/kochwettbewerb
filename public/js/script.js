78c103a121cdf65a93e6bc183a5c09c53427de20
Fabian Pottbäcker <fp@pottbaecker.de>
2016-04-30 15:37:48 +0200

    Added email validation

---
+++ public/js/script.js
@@ -0,0 +1,15 @@
+
+function validate (elem) {
+  var valid = true;
+  for(var i = 0; i < elem.children.length; i++){
+    var c = elem.children[i];
+    if (c.validity && !c.validity.valid){
+      c.classList.add("highlight");
+      window.setTimeout(function(c){
+        c.classList.remove("highlight");
+      }, 100, c);
+      valid = false;
+    }
+  }
+  return valid;
+}
